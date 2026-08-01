dc6dd4b302302edd7faf62609e51d7ede8ad4e1a
bharathreddybiradar-code <bharathreddybiradar@gmail.com>
2026-08-01 23:46:41 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Print Without Newline/print-without-newline.java
@@ -0,0 +1,11 @@
+class Solution {
+    public void utility(Scanner sc) {
+        
+        String a=sc.nextLine();
+        String b=sc.nextLine();
+
+        System.out.println(a+" "+b);
+    }
+    
+}
+
